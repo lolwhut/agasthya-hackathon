46c5a807835c72a1f7c293bed788079ab37dc42f
lourdesr <root.roashan@gmail.com>
2015-10-19 19:30:58 +0530

    Calculates Distance from Agastya

--- source.js
+++ source.js
@@ -1,19 +1,39 @@
 // Function to get the required data
 //LOLWHUT 
 
+
 function getData(){
 	var schools = [
+	    {"name":"Agastya","lng":50,"lat":50,"capacity":[300]},
 	{"name":"Gudipalli", "lng":5, "lat":10, "strength":[10,20,30,40,50]},
 	{"name":"Komanapalli", "lng":7, "lat":12, "strength":[20,15,40,12]}
+	    //Add More Schools Here
 	];
 
-return schools
+    return schools;
 };
 
 // Our data is here
 var schoolData = getData();
 
-console.log(getDistance(schoolData[0]+schoolData[1]));
+
+function objectLength(obj) {
+  var result = 0;
+  for(var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+    // or Object.prototype.hasOwnProperty.call(obj, prop)
+      result++;
+    }
+  }
+  return result;
+}
+
+
+for(var i=1;i<objectLength(schoolData);++i){
+
+console.log(getDistance(schoolData[0],schoolData[i])+"\n");
+
+}
 
 // Function to calculate the distance between two schools
 function getDistance(x,y)
@@ -24,6 +44,7 @@ function getDistance(x,y)
 	var long2 = y.lng;
 	var lat2 = y.lat;
 
-	var dist = 
-};
+    var dist = Math.sqrt((Math.pow((long1-long2),2)+Math.pow((lat1-lat2),2)));
 
+    return dist;
+};
