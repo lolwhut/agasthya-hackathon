4982981e705c1aa5208ea4376b2cc5af5fa49a7c
lourdesr <root.roashan@gmail.com>
2015-10-20 11:46:45 +0530

    Updated UI

--- uitests/script.js
+++ uitests/script.js
@@ -1,21 +1,43 @@
 var map;
+var infowindow;
+
 function initMap() {
- var spot = {lat: 12.977682, lng: 77.715176}
- 
- map = new google.maps.Map(document.getElementById('map'), {
-    center: spot,
+  var pyrmont = {lat: 12.8249489, lng: 78.2560428};
+    
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
     zoom: 15
   });
-    var marker = new google.maps.Marker({
 
-	position:spot,
-	map:map,
-	title:'Lul'
+  infowindow = new google.maps.InfoWindow();
 
-    });
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 10000,
+    types: ['school']
+  }, callback);
+}
 
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
 
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    console.log(placeLoc.lat());
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
